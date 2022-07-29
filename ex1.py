12e78ccd1caec4a672b278fc6ce7f3e5578ccced
steph-cs <61119367+steph-cs@users.noreply.github.com>
2022-07-29 15:07:45 -0300

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,70 +1,3 @@
-'''
-PROGRAM → STATEMENT |  FUNCLIST | ε 
-FUNCLIST → FUNCDEF FUNCLIST1
-FUNCLIST1 → FUNCLIST | ε 
-FUNCDEF → def ident ( PARAMLIST ){STATELIST} 
-TYPE → int | float | string 
-
-PARAMLIST → TYPE ident PARAMLIST1 | ε 
-PARAMLIST1 → , PARAMLIST | ε 
-
-STATEMENT → VARDECL ; | 
-ATRIBSTAT ; | 
-PRINTSTAT ; | 
-READSTAT ; | 
-RETURNSTAT ; | 
-IFSTAT | 
-FORSTAT | 
-{STATELIST} | 
-break; | ; 
-
-VARDECL → TYPE ident ARRAY1 
-ARRAY1 → [int_constant] | ε 
-
-ATRIBSTAT → LVALUE = ATRIB 
-ATRIB → EXPRESSION | FUNCCALL | ALLOCEXPRESSION 
-FUNCCALL → ident(PARAMLISTCALL)
-PARAMLISTCALL → ident PARAMLISTCALL1 | ε 
-PARAMLISTCALL1 → , PARAMLISTCALL | ε 
-PRINTSTAT → print EXPRESSION 
-READSTAT → read LVALUE
-
-RETURNSTAT → return RETURNSTAT1
-RETURNSTAT1 → ident | ε 
-LVALUE → ident OPT_NUMEXPRESSION
-OPT_NUMEXPRESSION → [NUMEXPRESSION] | ε
-
-IFSTAT → if(EXPRESSION) STATEMENT IFSTAT1 
-IFSTAT1 → else STATEMENT | ε
-
-FORSTAT → for(ATRIBSTAT; EXPRESSION; ATRIBSTAT)STATEMENT
-
-STATELIST → STATEMENT STATELIST1
-STATELIST1 → STATELIST | ε
-
-ALLOCEXPRESSION → new TYPE [ NUMEXPRESSION ]
-NUMEXPRESSION → TERM NUMEXPRESSION1 
-NUMEXPRESSION1 → OP1 TERM | ε
-OP1 → + | -
-OP2 → * | / | %
-TERM → UNARYEXPR TERM1
-TERM1 → OP2 UNARYEXPR | ε
- FACTOR → int_constant | 
-float_constant | 
-string_constant | 
-null |
-LVALUE |
-(NUMEXPRESSION)
-UNARYEXPR → OP_FACTOR FACTOR
-OP_FACTOR → OP1 | ε
-
-EXPRESSION → NUMEXPRESSION OPT_EXPRESSION
-OPT_EXPRESSION → OP NUMEXPRESSION | ε
-OP → < | > | <= | >= | == | !=
-
-'''
-
-
 
 #analise lexica
 reserved = {
@@ -80,14 +13,11 @@ reserved = {
     'else' : 'ELSE', 
     'for' : 'FOR', 
     'new' : 'NEW', 
-    'int_constant': 'INT_CONSTANT',
-    'float_constant':'FLOAT_CONSTANT', 
-    'string_constant':'STRING_CONSTANT', 
     'null' :'NULL'
  }
 
 
-tokens = ['IDENT'] + list(reserved.values())
+tokens = ['IDENT', 'INT_CONSTANT', 'FLOAT_CONSTANT','STRING_CONSTANT'] + list(reserved.values())
 
 literals = ['=', '<', '>', '!', '+', '-', '*', '/','%' , '(', ')', '[', ']', '{', '}' ,';', ',']
 
@@ -147,7 +77,7 @@ def p_FUNCLIST_2(p):
     pass
 
 def p_FUNCDEF(p):
-    """FUNCDEF : DEF IDENT '(' PARAMLIST ')' '[' STATELIST ']'"""  # noqa
+    """FUNCDEF : DEF IDENT '(' PARAMLIST ')' '{' STATELIST '}' """  # noqa
     pass
 
 def p_PARAMLIST(p):
@@ -178,7 +108,7 @@ def p_STATEMENT(p):
     | RETURNSTAT ';'
     | IFSTAT
     | FORSTAT
-    | '[' STATELIST ']'
+    | '{' STATELIST '}'
     | BREAK ';'
     | ';'"""
     pass
@@ -348,184 +278,11 @@ def p_EMPTY(p):
 erros_sin = []
 def p_error(p):
     if p:
-        erros_sin.append("Syntax error at '%s' \n" % p.value)
+        erros_sin.append("Syntax error at '%s'" % p.value)
     else:
         erros_sin.append("Syntax error at EOF")
 
-# def p_program(p):
-#     '''program : statement
-#                 | funclist
-#                 | empty'''
-        
-# def p_funclist(p):
-#     '''funclist : funcdef funclist1'''
 
-# def p_funclist1(p):
-#     '''funclist1 : funclist
-#                 | empty'''
-
-# def p_funcdef(p):
-#     '''funcdef : DEF IDENT '(' paramlist ')' '{' statelist '}' '''
-
-# def p_type(p):
-#     '''type : INT 
-#                 | FLOAT
-#                 | STRING'''
-
-# def p_paramlist(p):
-#     '''paramlist : type IDENT paramlist1
-#                 | empty'''
-
-# def p_paramlist1(p):
-#     '''paramlist1 : ',' paramlist
-#                 | empty'''
-
-# def p_statement(p):
-#     '''statement : vardecl ';'
-#                 | atribstat ';'
-#                 | printstat ';'
-#                 | readstat ';'
-#                 | returnstat ';'
-#                 | ifstat
-#                 | forstat
-#                 | '{' statelist '}'
-#                 | BREAK ';'
-#                 | ';' '''   
-
-# def p_vardecl(p):
-#     '''vardecl : type IDENT array1'''  
-
-# def p_array1(p):
-#     '''array1 : '[' INT_CONSTANT ']' 
-#                 | empty''' 
-
-# def p_atribstat(p):
-#     '''atribstat : lvalue '=' atrib'''
-
-# def p_atrib(p):
-#     '''atrib : expression 
-#                 | funccall
-#                 | allocexpression'''
-
-# def p_funccall(p):
-#     '''funccall : IDENT '(' paramlistcall ')' '''
-
-# def p_paramlistcall(p):
-#     '''paramlistcall : IDENT paramlistcall1
-#                 | empty'''
-
-# def p_paramlistcall1(p):
-#     '''paramlistcall1 : ',' paramlistcall 
-#                 | empty'''
-                
-# def p_printstat(p):
-#     '''printstat : PRINT expression '''
-
-# def p_readstat(p):
-#     '''readstat : READ lvalue '''
-
-# def p_returnstat(p):
-#     '''returnstat : RETURN returnstat1'''
-
-# def p_returnstat1(p):
-#     '''returnstat1 : IDENT
-#                 | empty'''
-
-# def p_lvalue(p):
-#     '''lvalue : IDENT opt_numexpression'''
-
-# def p_opt_numexpression(p):
-#     '''opt_numexpression : '[' numexpression ']'
-#                 | empty'''
-
-# def p_ifstat(p):
-#     '''ifstat : IF '(' expression ')' statement ifstat1
-#                 | empty'''
-
-# def p_ifstat1(p):
-#     '''ifstat1 : ELSE statement 
-#                 | empty'''
-
-# def p_forstat(p):
-#     '''forstat : FOR '(' atribstat ';' expression ';' atribstat ')' statement
-#                 | empty'''
-
-# def p_statelist(p):
-#     '''statelist : statement statelist1
-#                 | empty'''
-
-
-# def p_statelist1(p):
-#     '''statelist1 : statelist
-#                 | empty'''
-
-# def p_op1(p):
-#     '''op1 : '+'
-#                 | '-' '''
-
-# def p_op2(p):
-#     '''op2 : '*'
-#                 | '/' 
-#                 | '%' '''
-
-# def p_op(p):
-#     '''op : '<' 
-#                 | '>' 
-#                 | '<' '=' 
-#                 | '>' '=' 
-#                 | '=' '='
-#                 | '!' '=' '''
-
-# def p_allocexpression(p):
-#     '''allocexpression : NEW type '[' numexpression ']' '''
-
-# def p_numexpression(p):
-#     '''numexpression : term numexpression1'''
-
-# def p_numexpression1(p):
-#     '''numexpression1 : op1 term
-#                 | empty'''
-
-# def p_term(p):
-#     '''term : unaryexpr term1'''
-
-# def p_term1(p):
-#     '''term1 : op2 unaryexpr
-#                 | empty'''
-
-# def p_factor(p):
-#     '''factor : INT_CONSTANT
-#                 | FLOAT_CONSTANT
-#                 | STRING_CONSTANT
-#                 | NULL
-#                 | lvalue
-#                 | '(' numexpression ')' '''                             
-
-# def p_unaryexpr(p):
-#     '''unaryexpr : op_factor factor'''
-
-# def p_op_factor(p):
-#     '''op_factor : op1
-#                 | empty'''
-
-# def p_expression(p):
-#     '''expression : numexpression opt_expression '''
-
-# def p_opt_expression(p):
-#     '''opt_expression : op numexpression
-#                 | empty'''
-
-
-# def p_empty(p):
-#     'empty : '
-#     pass
-
-# erros_sin = []
-# def p_error(p):
-#     if p:
-#         erros_sin.append("Syntax error at '%s' \n" % p.value)
-#     else:
-#         erros_sin.append("Syntax error at EOF")
         
 
 import ply.yacc as yacc
@@ -534,6 +291,8 @@ parser = yacc.yacc()
 
 
 while True:
+    erros_lex = []
+    erros_sin = []
     try:
         s = input('cod > ')
     except EOFError:
@@ -558,4 +317,4 @@ while True:
         else:
             print("Erros sintaticos = '%d'" % len(erros_sin))
             for err in erros_sin:
-                print(err)    
+                print(err)  
